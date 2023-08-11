378792f420a0437dca3a43741a5224afa1d8cf51
mphhohku <130424415+mphhohku@users.noreply.github.com>
2023-08-11 16:40:21 +0800

    aug

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,14 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const toggleBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal-overlay');
+toggleBtn.addEventListener('click', function () {
+    modal.classList.add('open-modal');
+    }
+);
+closeBtn.addEventListener('click', function () {
+    modal.classList.remove('open-modal');
+    }
+);
--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,46 @@
 //using selectors inside the element
 // traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function () {
+        questions.forEach(function (item) {
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
+    });
+});
+
+
+// altnerative way to select elements
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function (btn) { 
+//     btn.addEventListener('click', function (e) {
+//         const question = e.currentTarget.parentElement.parentElement;   // currentTarget is the element that the event listener is attached to, parentElement is the parent of the current element
+//         console.log(e.currentTarget, e.currentTarget.parentElement, e.currentTarget.parentElement.parentElement);
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+
+// draft
+// const iconPlusBtn = document.querySelector('.plus-icon')
+// const iconMinusBtn = document.querySelector('.minus-icon')
+// const question = document.querySelector('.question')
+
+// iconPlusBtn.addEventListener('click', function () {
+//     iconPlusBtn.classList.toggle('show-text');
+//     iconMinusBtn.classList.toggle('show-text');
+//     question.classList.toggle('show-text');
+// });
+
+// iconMinusBtn.addEventListener('click', function () {
+//     iconPlusBtn.classList.toggle('show-text');
+//     iconMinusBtn.classList.toggle('show-text');
+//     question.classList.toggle('show-text');
+// });
